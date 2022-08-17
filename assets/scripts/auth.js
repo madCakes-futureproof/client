fe66a957f3c3018ddab4cbfd45aab83f519364df
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-08-18 00:12:39 +0100

    Add changes to auth

---
+++ assets/scripts/auth.js
@@ -0,0 +1,84 @@
+const API_URL = `http://localhost:3000/`;
+// 'https://hacker-health-tracker.herokuapp.com/';
+
+let userLoginForm = document.querySelector(".login");
+// let userRegisterForm = document.querySelector(".login-2")
+
+userLoginForm.addEventListener('submit', login);
+// userRegisterForm.addEventListener('submit', register);
+
+
+async function login (data) {
+    
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    await fetch(API_URL + 'users/login', options)
+        .then(res => res.json())
+        .then(data => {
+            if (data["success"]) {
+                localStorage.setItem("token", data["token"]);
+                window.location.assign("main-page.html");
+            } else {
+                throw "Unable to authenticate!"
+            }
+        })
+        .catch(err => alert(err))
+    }
+
+    document.querySelector(".login").addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        const form = new FormData(e.target);
+    
+        login({
+            username: form.get("username"),
+            password: form.get("password")
+        })
+    
+        e.target.reset();
+    })
+
+
+    // async function register (data) {
+    
+    //     const options = {
+    //         method: "POST",
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //     }
+    
+    //     await fetch(API_URL + 'users/register', options)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             if (data["success"]) {
+    //                 localStorage.setItem("token", data["token"]);
+    //                 // window.location.assign("/main-page.html");
+    //             } else {
+    //                 throw "Unable to register!"
+    //             }
+    //         })
+    //         .catch(err => alert(err))
+    //     }
+
+    //     document.querySelector(".login-2").addEventListener("submit", (e) => {
+    //         e.preventDefault();
+        
+    //         const form = new FormData(e.target);
+        
+    //         login({
+    //             username: form.get("username"),
+    //             password: form.get("password")
+    //         })
+        
+    //         e.target.reset();
+    //     })

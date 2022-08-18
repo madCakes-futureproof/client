96a1584aa12ba0563ac4172d066fd49c4884f02c
madCakes <jason.pbrooks@hotmail.com>
2022-08-18 15:52:36 +0100

    purge and restart

--- assets/scripts/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = "https://hacker-health-tracker.herokuapp.com/";
-export const API_LOGIN = "https://hackerhealthtracker.netlify.app/"
-export const API_REGISTER = "https://hacker-health-tracker.herokuapp.com/register"

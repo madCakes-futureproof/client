ad10c82491e16235600d6674cb055899505301e0
madCakes <jason.pbrooks@hotmail.com>
2022-08-18 16:44:44 +0100

    add displayHabit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,8 @@ console.log(newHabit)
 
 newHabit.addEventListener('submit', addHabit)
 
+const habits = {}
+
 async function addHabit(e) {
     e.preventDefault()
 
@@ -13,7 +15,7 @@ async function addHabit(e) {
         const repetitions = document.getElementById('repetitions').value
         const frequency = document.getElementById('frequency').value
 
-        const content = {
+        const habit = {
             name: name,
             repetitions: repetitions,
             frequency: frequency,
@@ -22,27 +24,49 @@ async function addHabit(e) {
             user_id: 1
         }
 
+        habits.push(habit)
+
         console.log(content)
 
+        
         const options = {
             method: "POST",
             headers: {
-            "Content-Type": "application/json",
+                "Content-Type": "application/json",
             }
         }
-
+        
         const response = await fetch("https://hacker-health-tracker.herokuapp.com/habits", options)
         const data = await response.json()
-
+        
         console.log(data)
 
+        displayHabit()
+        
     } catch (err){
         alert(err)
     }
 }
 
-function displayHabit() {
-    const markup = `
+displayHabit(habits)
+
+function displayHabit(habits) {
+
+    habits.forEach(habit => {
         
-    `        
+        
+    });
+    const markup = `
+        <li>
+        <input type="checkbox" ${
+    habit.completed ? "checked" : ""
+    } />
+        <label><span>${habit.repetitions}/${habit.repetitions} ${
+    habit.timeframe
+    }</span> ${habit.text}</label>
+    <p>Streak:${habit.streak}</p>
+    <button class="delete" ">Delete</button>
+    </li>`
+
+        
 }

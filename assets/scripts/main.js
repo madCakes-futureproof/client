c1c058c18cc05e38a0a8ab0aa2b0f57574aa318b
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-08-18 23:54:42 +0100

    Add counter

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,11 +60,11 @@ function displayHabit(habits) {
             const divContainer = document.createElement('div');
             divContainer.setAttribute('class', "divContainer");
            
-            const counter = document.createElement('div');
+            let counter = document.createElement('div');
             const reps = document.createElement('p');
                 reps.textContent ='Completed'
                 counter.setAttribute('class', "counter");
-                counter.textContent = 0 + '/';
+                counter.textContent = 0;
 
             const repetitions = document.createElement('div');              
                 repetitions.setAttribute('class', "repetitions");
@@ -87,10 +87,18 @@ function displayHabit(habits) {
 
             const incrementBtn = document.createElement('button');
             incrementBtn.setAttribute('class', "button");
+            incrementBtn.setAttribute('id', "buttonInc");
             incrementBtn.textContent = '+';
             const deleteButton = document.createElement('button');
             deleteButton.setAttribute('class', "button");
             deleteButton.textContent = 'Delete';
+
+            incrementBtn.addEventListener('click', (e) => {
+                counter.textContent = parseInt(counter.textContent) + 1;
+            })  
+
+            
+                   
 
             divContainer.appendChild(incrementBtn);
             divContainer.appendChild(counter);
@@ -100,65 +108,16 @@ function displayHabit(habits) {
             divContainer.appendChild(deleteButton);
 
         
-            habitContainer.appendChild(divContainer)
+            habitContainer.appendChild(divContainer);
 
         });
 
-       
-
-
-
-    //     let counter = 0;
-    //     return `
-    //         <li>
-    //         <input type="checkbox" id="counter" ${
-    //     habit.completed ? "checked" : ""
-    //     }/>
-    //         <label><span>${counter}/${habit.repetitions} ${
-    //     habit.frequency
-    //     }</span> ${habit.name}  </label>
-    //     <br/>
-    //     <p>  Streak:  ${habit.streak}</p>
-    //     <button class="delete" ">Delete</button>
-    //     </li>`
-            
-    // }).join()   
-
-    // const countEl = document.getElementById('counter');
-    // countEl.addEventListener('click', () => ++)
-// })
-
 }
 
-displayHabit()
+displayHabit();
 
-// function createHabitElement(b) {
-//     if (!b) {
-//       throw "No data provided";
-//     }
-  
-//     const elem = document.createElement("div");
-//     elem.className = "card"
-//     const t = document.createElement("div")
-//     t.className = "card-body"
-//     t.textContent = b;
-//     elem.append(t)
-//     return elem;
-  
-//   }
 
-// function collect() {
-//     fetch(`https://hacker-health-tracker.herokuapp.com/habits`)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(JSON.stringify(data))
-//         data.forEach((b) => {
-//           const x = createHabitElement(b);
-          
-//           document.querySelector(".habits").appendChild(x);
-//         });
-//       })
-//       .catch((err) => console.log(err));
-//   }
-//   collect();
-
+// function updateCount() {
+//     counter.textContent = parseInt(span.textContent) + 1;
+    
+// }

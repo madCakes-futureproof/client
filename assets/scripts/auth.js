4f7eb35e9867167dfb22856e76099417fe5ef19f
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-08-18 15:40:52 +0100

    Change auth

--- assets/scripts/auth.js
+++ assets/scripts/auth.js
@@ -1,49 +1,106 @@
-const API_URL = `http://localhost:3000/`;
-// 'https://hacker-health-tracker.herokuapp.com/';
 
-let userLoginForm = document.querySelector(".login");
+
+const API_URL = 'https://hacker-health-tracker.herokuapp.com/';
+// `http://localhost:3000/`
+
+
+const loginForm = document.querySelector('.login');
+const registerForm = document.querySelector('.login-1')
+
+loginForm.addEventListener('submit', login);
+registerForm.addEventListener('submit', register);
+
+async function login(e) {
+  e.preventDefault();
+  try {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+    };
+    
+    const res = await fetch(API_URL + 'users/login', options);
+    const data = await res.json();
+    if (!data.success) {
+      throw new Error('Can`t log in!');
+    }
+    loginData(data.token);
+    window.location.assign = 'main-page.html';
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+async function register(e) {
+  e.preventDefault();
+  try {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+    };
+    const res = await fetch(API_URL + 'users/register', options);
+    const data = await res.json();
+   
+    if (data.err) {
+      throw Error(data.err);
+    }
+    window.location.assign = 'main-page.html';
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+function loginData(token) {
+  const user = jwt_decode(token);
+  localStorage.setItem('token', token);
+  localStorage.setItem('username', users.username);
+  window.location.assign = 'main-page.html';
+}
+
+// let userLoginForm = document.querySelector(".login");
 // let userRegisterForm = document.querySelector(".login-2")
 
-userLoginForm.addEventListener('submit', login);
+// userLoginForm.addEventListener('submit', login);
 // userRegisterForm.addEventListener('submit', register);
 
 
-async function login (data) {
+// async function login (data) {
     
-    const options = {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//     }
 
-    await fetch(API_URL + 'users/login', options)
-        .then(res => res.json())
-        .then(data => {
-            if (data["success"]) {
-                localStorage.setItem("token", data["token"]);
-                window.location.assign("main-page.html");
-            } else {
-                throw "Unable to authenticate!"
-            }
-        })
-        .catch(err => alert(err))
-    }
+//     await fetch(API_URL + 'users/login', options)
+//         .then(res => res.json())
+//         .then(data => {
+//             if (data["success"]) {
+//                 localStorage.setItem("token", data["token"]);
+//                 window.location.assign("main-page.html");
+//             } else {
+//                 throw "Unable to authenticate!"
+//             }
+//         })
+//         .catch(err => alert(err))
+//     }
 
-    document.querySelector(".login").addEventListener("submit", (e) => {
-        e.preventDefault();
+//     document.querySelector(".login").addEventListener("submit", (e) => {
+//         e.preventDefault();
     
-        const form = new FormData(e.target);
+//         const form = new FormData(e.target);
     
-        login({
-            username: form.get("username"),
-            password: form.get("password")
-        })
+//         login({
+//             username: form.get("username"),
+//             password: form.get("password")
+//         })
     
-        e.target.reset();
-    })
+//         e.target.reset();
+//     })
 
 
     // async function register (data) {
--- assets/scripts/model.js
+++ assets/scripts/model.js
@@ -2,101 +2,129 @@ const addHabits = document.querySelector(".add-habit");
 const habitsList = document.querySelector(".habits");
 const habits = JSON.parse(localStorage.getItem("habits")) || [];
 
-let habit = {}
+function newHabit(habit) {
 
-function addHabit(e) {
-  e.preventDefault();
-  const text = this.querySelector("[name=habit]").value;
-  const totalCounts = this.querySelector("[name=reps]").value;
-  const timeframe = this.querySelector("[name=timeframe]").value;
+  const elem = document.createElement("div");
+  const div = document.createElement("div")
+  div.textContent = habit;
+  elem.append(div)
 
-  habit = {
-    text: text,
-    reps: 0,
-    totalCounts: totalCounts,
-    timeframe: timeframe,
-    completed: false,
-    streak: 0
-  };
+  return elem;
 
-  habits.push(habit);
-  listHabits(habits, habitsList);
-  localStorage.setItem("habits", JSON.stringify(habits));
-  this.reset();
-  console.log(habit);
 }
 
-function listHabits(habit = [], habitsList) {
-  habitsList.innerHTML = habits
-    .map((habit, i) => {
-      return `
-            <li>
-            <input type="checkbox" data-index=${i} id="habit${i}" ${
-        habit.completed ? "checked" : ""
-      } />
-            <label for="habit${i}"><span>${habit.reps}/${habit.totalCounts} ${
-        habit.timeframe
-      }</span> ${habit.text}</label>
-      <p>Streak:${habit.streak}</p>
-        <button class="delete" data-index=${i} id="delete${i}">Delete</button>
-        </li>
-        `;
+function displayHabits() {
+  fetch(`https://hacker-health-tracker.herokuapp.com/habits`)
+    .then((res) => res.json)
+    .then((data) => {
+      console.log(data)
+      data.forEach((habit) => {
+        
+        const res = newHabit(habit);
+        
+        document.querySelector("#habits").appendChild(res);
+      });
     })
-    .join("");
+    .catch((err) => console.log(err));
 }
+displayHabits();
 
-// Toggle If Complete
-function toggleCompleted(e) {
-  if (!e.target.matches("input")) return;
-  const el = e.target;
-  const index = el.dataset.index;
-  habits[index].reps += 1;
+addHabits.addEventListener(displayHabits)
+// let habit = {}
 
-  if (habits[index].reps > habits[index].totalCounts) {
-    habits[index].reps = 0;
-    habits[index].completed = false;
-  } 
-  if (habits[index].reps === habits[index].totalCounts) {
-    habits[index].completed = true;
-    incrementStreak()
-    habits[index].reps = 0
-  }
+// function addHabit(e) {
+//   e.preventDefault();
+//   const text = this.querySelector("[name=habit]").value;
+//   const totalCounts = this.querySelector("[name=reps]").value;
+//   const timeframe = this.querySelector("[name=timeframe]").value;
 
-  listHabits(habits, habitsList);
-  localStorage.setItem("habits", JSON.stringify(habits));
-}
+//   habit = {
+//     text: text,
+//     reps: 0,
+//     totalCounts: totalCounts,
+//     timeframe: timeframe,
+//     completed: false,
+//     streak: 0
+//   };
 
-function incrementStreak() {
-    let counter = 0
-    counter++
+//   habits.push(habit);
+//   listHabits(habits, habitsList);
+//   localStorage.setItem("habits", JSON.stringify(habits));
+//   this.reset();
+//   console.log(habit);
+// }
 
-    habit.streak = counter
-    return habit.streak
-    // display streak
-}
-const showStreak = function() {
-    return 
-}
+// function listHabits(habit = [], habitsList) {
+//   habitsList.innerHTML = habits
+//     .map((habit, i) => {
+//       return `
+//             <li>
+//             <input type="checkbox" data-index=${i} id="habit${i}" ${
+//         habit.completed ? "checked" : ""
+//       } />
+//             <label for="habit${i}"><span>${habit.reps}/${habit.totalCounts} ${
+//         habit.timeframe
+//       }</span> ${habit.text}</label>
+//       <p>Streak:${habit.streak}</p>
+//         <button class="delete" data-index=${i} id="delete${i}">Delete</button>
+//         </li>
+//         `;
+//     })
+//     .join("");
+// }
 
-function displayStreak() {
+// // Toggle If Complete
+// function toggleCompleted(e) {
+//   if (!e.target.matches("input")) return;
+//   const el = e.target;
+//   const index = el.dataset.index;
+//   habits[index].reps += 1;
+
+//   if (habits[index].reps > habits[index].totalCounts) {
+//     habits[index].reps = 0;
+//     habits[index].completed = false;
+//   } 
+//   if (habits[index].reps === habits[index].totalCounts) {
+//     habits[index].completed = true;
+//     incrementStreak()
+//     habits[index].reps = 0
+//   }
+
+//   listHabits(habits, habitsList);
+//   localStorage.setItem("habits", JSON.stringify(habits));
+// }
+
+// function incrementStreak() {
+//     let counter = 0
+//     counter++
+
+//     habit.streak = counter
+//     return habit.streak
+//     // display streak
+// }
+// const showStreak = function() {
+//     return 
+// }
+
+// function displayStreak() {
     
-}
+// }
 
 
-// Delete Habit
-function deleteHabit(e) {
-  if (!e.target.matches("button")) return;
-  const el = e.target;
-  const index = el.dataset.index;
+// // Delete Habit
+// function deleteHabit(e) {
+//   if (!e.target.matches("button")) return;
+//   const el = e.target;
+//   const index = el.dataset.index;
 
-  habits.splice(index, 1);
+//   habits.splice(index, 1);
 
-  listHabits(habits, habitsList);
-  localStorage.setItem("habits", JSON.stringify(habits));
-}
+//   listHabits(habits, habitsList);
+//   localStorage.setItem("habits", JSON.stringify(habits));
+// }
 
-addHabits.addEventListener("submit", addHabit);
-habitsList.addEventListener("click", toggleCompleted);
-habitsList.addEventListener("click", deleteHabit);
+// addHabits.addEventListener("submit", addHabit);
+// habitsList.addEventListener("click", toggleCompleted);
+// habitsList.addEventListener("click", deleteHabit);
 
-listHabits(habits, habitsList);
+// listHabits(habits, habitsList);
